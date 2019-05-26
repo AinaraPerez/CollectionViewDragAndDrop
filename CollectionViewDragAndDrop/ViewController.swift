761509da6a09cdf91f676faac64c3613ccd38c66
AinaraPerez <ainara.pa85@gmail.com>
2019-05-26 17:42:51 +0200

    Initial Commit

---
+++ CollectionViewDragAndDrop/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CollectionViewDragAndDrop
+//
+//  Created by Ainara Perez on 26/5/19.
+//  Copyright © 2019 Ainara Perez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
